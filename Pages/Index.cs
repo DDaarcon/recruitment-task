6cb448de0dd850d15f29baf3e42cbf94bc22358a
DDaarcon <dda.darek@gmail.com>
2021-04-17 05:19:24 +0200

    added preliminary turn handling

--- Pages/Index.cs
+++ Pages/Index.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Components;
 using ZadanieRekrutacyjne.Classes;
 
@@ -5,10 +6,35 @@ namespace ZadanieRekrutacyjne.Pages {
 
 	public class IndexClass : ComponentBase {
 
-		public Stage stage1 = new Stage();
-		public Stage stage2 = new Stage();
+		bool firstPlayerTurn = true;
+		public Stage stage1;
+		public Stage stage2;
+
+		private int[] ships = {
+			5, 4, 3, 3, 2
+		};
 		protected override void OnInitialized() {
 			
+			stage1 = new Stage(ships);
+			stage2 = new Stage(stage1, ships);
+			stage1.opponentsStage = stage2;
+		}
+
+		public void NextMove() {
+			var random = new Random();
+			int xPos, yPos;
+			if (firstPlayerTurn) {
+				do {
+					xPos = random.Next(10);
+					yPos = random.Next(10);
+				} while (!stage1.DealAttack(xPos, yPos));
+			} else {
+				do {
+					xPos = random.Next(10);
+					yPos = random.Next(10);
+				} while (!stage2.DealAttack(xPos, yPos));
+			}
+			firstPlayerTurn = !firstPlayerTurn;
 		}
 	}
 
