0c12962294d8eb27f5fe5407fcc733c5927bc5de
DDaarcon <dda.darek@gmail.com>
2021-04-17 12:20:38 +0200

    improved AI and added finished game check in Index.cs

--- Classes/AI.cs
+++ Classes/AI.cs
@@ -6,16 +6,21 @@ namespace ZadanieRekrutacyjne.Classes {
 		struct Coords {
 			public int x;
 			public int y;
+
+			public Coords(int x, int y) {
+				this.x = x;
+				this.y = y;
+			}
 		}
 
-		private int CompareCoords(Coords coords1, Coords coords2, Ship.Direction direction) {
-			if (direction == Ship.Direction.Horizontal) {
-				return coords1.x.CompareTo(coords2.x);
-			}
-			else if (direction == Ship.Direction.Vertical) {
-				return coords2.y.CompareTo(coords2.y);
-			}
-		}
+		// private int CompareCoords(Coords coords1, Coords coords2, Ship.Direction direction) {
+		// 	if (direction == Ship.Direction.Horizontal) {
+		// 		return coords1.x.CompareTo(coords2.x);
+		// 	}
+		// 	else if (direction == Ship.Direction.Vertical) {
+		// 		return coords2.y.CompareTo(coords2.y);
+		// 	}
+		// }
 
 		// private class Ship {
 		// 	public enum Direction {Horizontal, Vertical, Unknown}
@@ -28,8 +33,8 @@ namespace ZadanieRekrutacyjne.Classes {
 
 		public Stage ownStage;
 		// private Ship[] foundEnemyShips;
-		private List<Coords> alreadyHit;
-		private List<Coords> possibleTargets;
+		private List<Coords> alreadyHit = new List<Coords>();
+		private Queue<Coords> possibleTargets = new Queue<Coords>();
 		public void DealBetterAttack() {
 			if (ownStage == null) return;
 
@@ -50,8 +55,56 @@ namespace ZadanieRekrutacyjne.Classes {
 			// 	}
 			// }
 
-			// have not found undestroyed enemy ship, shoot randomly
+			Coords coords = new Coords(0, 0);
+			Stage.ShotState result;
+			bool gotFromPossibleTargets = false;
 
+			if (possibleTargets.Count > 0) {
+				do {
+					if (possibleTargets.Count <= 0) break;
+					coords = possibleTargets.Dequeue();
+					gotFromPossibleTargets = ownStage.DealAttack(coords.x, coords.y);
+				} while (!gotFromPossibleTargets);
+			}
+
+			if (!gotFromPossibleTargets) {
+				Random random = new Random();
+
+				do {
+					coords.x = random.Next(10);
+					coords.y = random.Next(10);
+				} while (!ownStage.DealAttack(coords.x, coords.y));
+			}
+
+			result = ownStage.opponentsStage.shotBoard[coords.x, coords.y];
+
+			// ship hit, add possible targets
+			if (result == Stage.ShotState.Shot) {
+
+				Coords nextPossibleTarget = new Coords(coords.x - 1, coords.y);
+				if (coords.x > 0) {
+					possibleTargets.Enqueue(nextPossibleTarget);
+				}
+				nextPossibleTarget = new Coords(coords.x, coords.y - 1);
+				if (coords.y > 0) {
+					possibleTargets.Enqueue(nextPossibleTarget);
+				}
+				nextPossibleTarget = new Coords(coords.x + 1, coords.y);
+				if (coords.x < 9) {
+					possibleTargets.Enqueue(nextPossibleTarget);
+				}
+				nextPossibleTarget = new Coords(coords.x, coords.y + 1);
+				if (coords.y < 9) {
+					possibleTargets.Enqueue(nextPossibleTarget);
+				}
+			}
+
+			return;
+		}
+
+
+		public AI(Stage ownStage) {
+			this.ownStage = ownStage;
 		}
 	}
 }
--- Pages/Index.cs
+++ Pages/Index.cs
@@ -9,6 +9,7 @@ namespace ZadanieRekrutacyjne.Pages {
 		bool firstPlayerTurn = true;
 		public Stage stage1;
 		public Stage stage2;
+		private AI ai1, ai2;
 
 		private int[] ships = {
 			5, 4, 3, 3, 2
@@ -17,23 +18,33 @@ namespace ZadanieRekrutacyjne.Pages {
 			
 			stage1 = new Stage(ships);
 			stage2 = new Stage(stage1, ships);
+			ai1 = new AI(stage1);
+			ai2 = new AI(stage2);
 			stage1.opponentsStage = stage2;
 		}
 
 		public void NextMove() {
-			var random = new Random();
-			int xPos, yPos;
+			// var random = new Random();
+			// int xPos, yPos;
+			// if (firstPlayerTurn) {
+			// 	do {
+			// 		xPos = random.Next(10);
+			// 		yPos = random.Next(10);
+			// 	} while (!stage1.DealAttack(xPos, yPos));
+			// } else {
+			// 	do {
+			// 		xPos = random.Next(10);
+			// 		yPos = random.Next(10);
+			// 	} while (!stage2.DealAttack(xPos, yPos));
+			// }
+			if (stage1.allShipsSunk || stage2.allShipsSunk) return;
+
 			if (firstPlayerTurn) {
-				do {
-					xPos = random.Next(10);
-					yPos = random.Next(10);
-				} while (!stage1.DealAttack(xPos, yPos));
+				ai1.DealBetterAttack();
 			} else {
-				do {
-					xPos = random.Next(10);
-					yPos = random.Next(10);
-				} while (!stage2.DealAttack(xPos, yPos));
+				ai2.DealBetterAttack();
 			}
+
 			firstPlayerTurn = !firstPlayerTurn;
 		}
 	}
